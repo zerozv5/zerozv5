a2d5cf6b7f2326d88dafce19f647184b6a1f902e
Zeroz V5 <zeroz.vi5@gmail.com>
2025-02-26 05:54:55 +0700

    Create script.js

---
+++ PortoNeon/script.js
@@ -0,0 +1,5 @@
+// Fade-in animation on load
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector(".container").style.opacity = "1";
+    // Tambah efek lain di sini kalau mau (contoh: console.log("Portfolio loaded!"))
+});
