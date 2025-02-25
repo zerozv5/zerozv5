b8dfc3335c2f4f9c854aec9c042578e66d77e4aa
Zeroz V5 <zeroz.vi5@gmail.com>
2025-02-26 06:01:33 +0700

    Delete PortoNeon directory

--- PortoNeon/script.js
+++
@@ -1,5 +0,0 @@
-// Fade-in animation on load
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector(".container").style.opacity = "1";
-    // Tambah efek lain di sini kalau mau (contoh: console.log("Portfolio loaded!"))
-});
